c34c1c155d8f589cc338716e18cf9b55272df477
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-02-14 21:31:03 +0330

    test: add `min` rule test

--- src/rules/min.ts
+++ src/rules/min.ts
@@ -3,8 +3,12 @@ import { Rule } from '@/types';
 function min(value: string, min: string): true | Error {
   const minInNumber = Number(min);
 
-  if (Number.isNaN(minInNumber)) {
+  if (min === '' || Number.isNaN(minInNumber)) {
     throw new Error('min rule expects a number as argument');
+  }
+
+  if (minInNumber < 0) {
+    throw new Error('min rule expects a positive number as argument');
   }
 
   return value.length >= minInNumber || new Error(`Min length is ${min}`);
--- tests/rules/min.test.ts
+++ tests/rules/min.test.ts
@@ -1,5 +1,27 @@
-import { assert, expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { min } from '@/rules';
 
-test('Input is filled', () => {
-  //
+describe('rules: min', () => {
+  it('valid value', () => {
+    expect(min('abc', '2')).toBe(true);
+    expect(min('abc', '3')).toBe(true);
+    expect(min('another', '5')).toBe(true);
+    expect(min('equal', '5')).toBe(true);
+    expect(min('text with space', '15')).toBe(true);
+  });
+
+  it('invalid value', () => {
+    expect(min('abc', '4')).instanceOf(Error);
+    expect(min('abc', '5')).instanceOf(Error);
+    expect(min('another', '10')).instanceOf(Error);
+    expect(min('equal', '6')).instanceOf(Error);
+    expect(min('text with space', '16')).instanceOf(Error);
+  });
+
+  it('invalid argument', () => {
+    expect(() => min('abc')).toThrowError();
+    expect(() => min('abc', '')).toThrowError();
+    expect(() => min('abc', '-1')).toThrowError();
+    expect(() => min('abc', 'text')).toThrowError();
+  });
 });
