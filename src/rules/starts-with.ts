03895090fd77d262851e66c1aca65a080ed2f4ee
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-02-15 16:18:26 +0330

    fix: fix incorrect error texts

--- src/rules/starts-with.ts
+++ src/rules/starts-with.ts
@@ -2,7 +2,7 @@ import { Rule } from '@/types';
 import { throwErrorIfArgsNotProvided } from '@/utils/checker';
 
 function startsWith(value: string, args: string): true | Error {
-  throwErrorIfArgsNotProvided(args, 'starts-with rule expects at least one argument');
+  throwErrorIfArgsNotProvided(args, 'starts-with rule expects one argument');
 
   return value.startsWith(args) || new Error(`The value must starts with ${args}`);
 }
