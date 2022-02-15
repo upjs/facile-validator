a314deaac2ca0d5b2da07ac60519128b9101c098
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-02-15 20:22:05 +0330

    perf: value calculation moved to the outside the loop

--- src/Validator.ts
+++ src/Validator.ts
@@ -19,6 +19,8 @@ class Validator {
           const givenRules = input.getAttribute('data-rules')?.split('|');
 
           if (givenRules) {
+            const value = getValue(input);
+
             for (const givenRule of givenRules) {
               let rule = givenRule;
               let args = '';
@@ -28,7 +30,6 @@ class Validator {
               }
 
               rule = toCamelCase(rule);
-              const value = getValue(input);
 
               if (rule in rules) {
                 const result = (rules as Rules)[rule](value, args);
