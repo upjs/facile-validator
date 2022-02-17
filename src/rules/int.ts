90a876cb5280cf8bfd6cd02726762d4d2754b23a
AliN11 <ms.kinect@yahoo.com>
2022-02-17 17:03:28 +0330

    fix: rename regexes

--- src/rules/int.ts
+++ src/rules/int.ts
@@ -1,7 +1,7 @@
 import { Rule } from '@/types';
 import { RuleError } from '@/modules/rule-error';
 import { INTEGER } from '@/types/error-cause';
-import { integerRegex } from '@/utils/regex';
+import { integer as integerRegex } from '@/utils/regex';
 
 function int(value: string): true | RuleError {
   return integerRegex.test(value) || new RuleError('int', INTEGER);
