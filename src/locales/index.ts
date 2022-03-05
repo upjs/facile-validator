f89680274a4159d8552f5b2bb390af601128d421
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-03-05 10:43:10 +0330

    feat: add `createLang` function to support ts in custom langs

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,7 +1,6 @@
-import { LangKeys } from '@/types';
+import { Lang } from '@/types';
 
 export { default as en } from './en';
 export { default as fa } from './fa';
 
-type Lang = Partial<Record<LangKeys, string>>;
 export const createLang = (lang: Lang): Lang => lang;
