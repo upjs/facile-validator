f0bd026df6dc39d36bc384fefb0c38b47bc9482f
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-03-05 08:39:02 +0330

    chore: add online demo

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -6,4 +6,6 @@ export default defineConfig({
   ...viteConfig,
   root: resolve(__dirname, './'),
   publicDir: resolve(__dirname, './public'),
+  build: undefined,
+  base: 'https://upjs.github.io/easy-html-form-validator/',
 });
