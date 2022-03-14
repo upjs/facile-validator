ea31136b4cecc71dd2171452525b7c025c5e3c77
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-03-14 10:05:41 +0330

    test: add `int` rule tests

---
+++ tests/rules/int.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+import { int } from '@/rules';
+
+describe('rules: int', () => {
+  it('should accept for intbetic characters', () => {
+    expect(int('123')).toBe(true);
+    expect(int('-123')).toBe(true);
+    expect(int('+123')).toBe(true);
+    expect(int('0')).toBe(true);
+    expect(int('10')).toBe(true);
+    expect(int('-10')).toBe(true);
+    expect(int('+0')).toBe(true);
+    expect(int('-0')).toBe(true);
+  });
+
+  it('should reject for non-intbetic characters', () => {
+    expect(int('123a')).toBeInstanceOf(Error);
+    expect(int('a123 ')).toBeInstanceOf(Error);
+    expect(int('-a')).toBeInstanceOf(Error);
+    expect(int('+a')).toBeInstanceOf(Error);
+    expect(int('123a')).toBeInstanceOf(Error);
+    expect(int('1.2')).toBeInstanceOf(Error);
+    expect(int('-1.2')).toBeInstanceOf(Error);
+    expect(int('e')).toBeInstanceOf(Error);
+    expect(int('0.5')).toBeInstanceOf(Error);
+    expect(int('-0.5')).toBeInstanceOf(Error);
+  });
+});
