89ab3987640fbc81654f3807aa2ebd1d202f61c7
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-03-14 10:34:12 +0330

    test: add more test to `min` rule

--- tests/rules/min.test.ts
+++ tests/rules/min.test.ts
@@ -2,14 +2,30 @@ import { describe, expect, it } from 'vitest';
 import { min } from '@/rules';
 
 describe('rules: min', () => {
-  it('should accept value with correct input', () => {
+  it('should accept value with correct input (string type)', () => {
+    expect(min('abc', 'string,2')).toBe(true);
+    expect(min('abc', 'string,3')).toBe(true);
+    expect(min('another', 'string,6')).toBe(true);
+    expect(min('equal', 'string,5')).toBe(true);
+    expect(min('text with space', 'string,15')).toBe(true);
+  });
+
+  it('should accept value with correct input (number type)', () => {
     expect(min('150', 'number,150')).toBe(true);
     expect(min('100', 'number,50')).toBe(true);
     expect(min('-100', 'number,-200')).toBe(true);
     expect(min('-10', 'number,-10')).toBe(true);
   });
 
-  it('should reject value with incorrect input', () => {
+  it('should reject value with incorrect input (string type)', () => {
+    expect(min('abc', 'string,4')).instanceOf(Error);
+    expect(min('abc', 'string,5')).instanceOf(Error);
+    expect(min('another', 'string,8')).instanceOf(Error);
+    expect(min('equal', 'string,6')).instanceOf(Error);
+    expect(min('text with space', 'string,17')).instanceOf(Error);
+  });
+
+  it('should reject value with incorrect input (number type)', () => {
     expect(min('100', 'number,101')).instanceOf(Error);
     expect(min('50', 'number,100')).instanceOf(Error);
     expect(min('1', 'number,2')).instanceOf(Error);
