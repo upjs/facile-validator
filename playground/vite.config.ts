44769b997b12348fb73bab9a8bff301fc4c1afaf
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-04-14 23:36:32 +0430

    feat: add UnoCSS to playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,5 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import Unocss from 'unocss/vite';
+import presetWind from '@unocss/preset-wind';
 
 export default defineConfig({
   root: resolve(__dirname, './'),
@@ -10,4 +12,26 @@ export default defineConfig({
     },
   },
   base: 'https://upjs.github.io/facile-validator/',
+  plugins: [
+    Unocss({
+      presets: [presetWind()],
+      theme: {
+        colors: {
+          primary: '#ef233c',
+          heading: '#023047',
+        },
+        fontFamily: {
+          roboto: ['Roboto, sans-serif'],
+        },
+      },
+      shortcuts: {
+        input:
+          'mt-1 bg-gray-50 rounded-md px-3 py-1.5 transition transition-all outline-transparent focus:outline-primary border border-gray-100 focus:border-primary placeholder:text-sm text-heading',
+        label: 'ml-2 text-heading text-sm font-medium',
+        'label-not-inner': 'text-heading text-sm font-medium',
+        'form-control': 'flex flex-col w-full',
+        'validator-err': 'text-primary text-xs mt-2 ml-2',
+      },
+    }),
+  ],
 });
